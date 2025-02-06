39acdea87876415aa5bb6a217c5de9a638e45f1c
muhammad92 <muhammadattamimi@gmail.com>
2025-02-06 16:14:23 +0700

    dev done

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Typed from "typed.js"
+
+const typed = new Typed("#heading", {
+    strings: ["Software Engineer","Product Engineering", "AI Enthusiast"],
+    loop: true,
+    typespeed:70
+})
